df5c761306e222aafa81b778609add2585a8db09
Jullia Saad <julliasaad01@gmail.com>
2018-01-02 16:26:56 -0200

    Add startbase application

---
+++ applications/starbase/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <div class="container">
+      <div class="row">
+          <Character v-for="(id, index) in initial_ids" v-bind:id="id" key="index"></Character>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Character from './components/Character.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Generate Your Team',
+      initial_ids: [1, 13, 14]
+    }
+  },
+  components: {
+    Character
+  }
+}
+</script>
